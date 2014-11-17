2203f931ba04a2719e8b1f454589648e71b8abb3
afifmucholis <afif.mucholis@gmail.com>
2014-11-17 17:05:20 +0700

    view daftar barang

--- application/views/daftarBarang.php
+++ application/views/daftarBarang.php
@@ -1,18 +1,27 @@
 <p> jadi nanti semua yang ada hubungannnya dengan front end, berada di view, tidak ada aturan untuk memberinamanya</p>
 
-<h2> ini daftar barang </h2>
+<h2> daftar penitipan barang </h2>
+<?php if ($barang) { ?>
 <table>
-	<th>id</th>
-	<th>Nama</th>
-	<th>keterangan</th>
+	<th>No Transaksi</th>
+	<th>Nama Pemilik</th>
+	<th>Nomor HP</th>
+	<th>Deskripsi</th>
 	<?php
 			//karena yang diakses $data['barang'] cuma namanya
-	foreach ($barang as $item) {
-		echo "<tr>
-					<td> $item[0] </td>
-					<td> $item[1] </td>
-					<td> $item[2] </td>
-		</tr>";
+	foreach($barang as $item) {
+		echo "<tr>";
+					echo "<td>". $item['id_transaksi']. "</td>";
+					echo "<td>". $item['nama_pengguna']. "</td>";
+					echo "<td>" . $item['no_hp'] . " </td>";
+					echo "<td>" .$item['keterangan'] . "</td>";
+		echo "</tr>";
 	}
+
 	?>
 </table>
+<?php 
+	}else{
+		echo "data kosong";
+	}
+?>
